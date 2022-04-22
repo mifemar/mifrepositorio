0968101b49eb015fb126bfeaa64818bd8c07a539
mifemar <103223858+mifemar@users.noreply.github.com>
2022-04-22 10:06:47 +0200

    Delete script.php

--- script.php
+++
@@ -1,7 +0,0 @@
-<?php
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: GET,POST');
-header('Access-Control-Allow-Headers: X-Requested-With');
-$json = file_get_contents('https://eimtcms.eimt.uoc.edu/~mfernandezmarti/db.json?posts');
-echo $json;
-?>
